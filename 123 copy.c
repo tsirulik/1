96b9f80098064f7e8b741bf82e916a330824952f
tsirulik <146517372+tsirulik@users.noreply.github.com>
2023-11-18 13:43:55 +0500

    Add files via upload

---
+++ 123 copy.c
@@ -0,0 +1,7 @@
+// echo "# 1.0" >> README.md 
+//   git init 
+//   git add README.md 
+//   git commit -m "первая фиксация" 
+//   git ветка -M main 
+//   git удаленное добавление источника https://github.com/tsirulik/1.0.git
+//    git push - ты родной, главный
