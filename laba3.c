d8a36deda5609d77f96be4834ffed6acab3c1adc
tsirulik <146517372+tsirulik@users.noreply.github.com>
2023-12-26 14:51:59 +0500

    Add files via upload

---
+++ laba3.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Функция для проверки, является ли число простым
+int isPrime(int num) {
+    if (num <= 1) {
+        return 0;
+    }
+
+    for (int i = 2; i < num; i++) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+// Функция для поиска простых элементов массива и их обработки
+void processArray(int* arr, int size) {
+    int firstIndex = -1;
+    int secondIndex = -1;
+    int primeCount = 0;
+
+    // Поиск простых элементов и их индексов
+    for (int i = 0; i < size; i++) {
+        if (isPrime(arr[i])) {
+            primeCount++;
+            if (firstIndex == -1) {
+                firstIndex = i;
+            } else if (secondIndex == -1) {
+                secondIndex = i;
+            }
+        }
+    }
+
+    // Обработка найденных простых элементов
+    if (primeCount == 1) {
+        printf("Простой элемент один, меняем местами:\n");
+        int temp = arr[firstIndex];
+        arr[firstIndex] = arr[size - 1];
+        arr[size - 1] = temp;
+    } else if (primeCount == 2) {
+        printf("Простых элементов два, меняем их местами:\n");
+        int temp = arr[firstIndex];
+        arr[firstIndex] = arr[secondIndex];
+        arr[secondIndex] = temp;
+    } else {
+        printf("Простых элементов нет или их больше двух, выводим их индексы:\n");
+        printf("Индексы простых элементов: %d, %d\n", firstIndex, secondIndex);
+    }
+
+    // Вывод массива после обработки
+    printf("Массив после обработки:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int size;
+
+    printf("Введите количество элементов массива: ");
+    scanf("%d", &size);
+
+    int* arr = (int*)malloc(size * sizeof(int));
+
+    // Заполнение массива случайными числами
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 100 + 1; // Сгенерировать случайное число от 1 до 1000
+    }
+
+    // Вывод исходного массива
+    printf("Исходный массив:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    // Обработка массива
+    processArray(arr, size);
+
+    free(arr);
+    return 0;
+}
