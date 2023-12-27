2871b0fc9eecba0cc5acb73a8ac5796ec401342d
tsirulik <146517372+tsirulik@users.noreply.github.com>
2023-12-27 10:27:41 +0500

    Add files via upload

--- laba3.c
+++ laba3.c
@@ -1,86 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 // Функция для проверки, является ли число простым
-int isPrime(int num) {
+bool isPrime(int num) {
     if (num <= 1) {
-        return 0;
+        return false;
     }
-
-    for (int i = 2; i < num; i++) {
+    
+    for (int i = 2; i * i <= num; i++) {
         if (num % i == 0) {
-            return 0;
+            return false;
         }
     }
-
-    return 1;
+    
+    return true;
 }
 
-// Функция для поиска простых элементов массива и их обработки
-void processArray(int* arr, int size) {
-    int firstIndex = -1;
-    int secondIndex = -1;
-    int primeCount = 0;
-
-    // Поиск простых элементов и их индексов
+// Функция для поиска простых элементов в массиве
+void findPrimes(int arr[], int size) {
+    // Переменная для хранения количества простых элементов
+    int count = 0;
+    
+    // Массив для хранения индексов простых элементов
+    int indexes[size];
+    
+    // Перебираем элементы массива
     for (int i = 0; i < size; i++) {
         if (isPrime(arr[i])) {
-            primeCount++;
-            if (firstIndex == -1) {
-                firstIndex = i;
-            } else if (secondIndex == -1) {
-                secondIndex = i;
-            }
+            // Если элемент является простым, увеличиваем счетчик
+            count++;
+            
+            // Сохраняем индекс простого элемента
+            indexes[count-1] = i;
         }
     }
-
-    // Обработка найденных простых элементов
-    if (primeCount == 1) {
-        printf("Простой элемент один, меняем местами:\n");
-        int temp = arr[firstIndex];
-        arr[firstIndex] = arr[size - 1];
-        arr[size - 1] = temp;
-    } else if (primeCount == 2) {
-        printf("Простых элементов два, меняем их местами:\n");
-        int temp = arr[firstIndex];
-        arr[firstIndex] = arr[secondIndex];
-        arr[secondIndex] = temp;
-    } else {
-        printf("Простых элементов нет или их больше двух, выводим их индексы:\n");
-        printf("Индексы простых элементов: %d, %d\n", firstIndex, secondIndex);
+    
+    if (count == 1) {
+        // Если простой элемент один, выводим его на экран
+        printf("Простой элемент: %d\n", arr[indexes[0]]);
+    } else if (count == 2) {
+        // Если простых элементов два, меняем их местами
+        int temp = arr[indexes[0]];
+        arr[indexes[0]] = arr[indexes[1]];
+        arr[indexes[1]] = temp;
+        printf("Простые элементы поменялись местами.\n");
+    } else if (count > 2) {
+        // Если простых элементов больше двух, выводим все индексы
+        printf("Индексы простых элементов: ");
+        for (int i = 0; i < count; i++) {
+            printf("%d ", indexes[i]);
+        }
+        printf("\n");
     }
-
-    // Вывод массива после обработки
-    printf("Массив после обработки:\n");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
 }
 
 int main() {
     int size;
-
+    
+    // Запрос количества элементов массива
     printf("Введите количество элементов массива: ");
     scanf("%d", &size);
-
-    int* arr = (int*)malloc(size * sizeof(int));
-
-    // Заполнение массива случайными числами
+    
+    // Создание массива и заполнение случайными числами
+    int arr[size];
     for (int i = 0; i < size; i++) {
-        arr[i] = rand() % 100 + 1; // Сгенерировать случайное число от 1 до 1000
+        arr[i] = rand() % 1000;
     }
-
+    
     // Вывод исходного массива
-    printf("Исходный массив:\n");
+    printf("Исходный массив: ");
     for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
-
-    // Обработка массива
-    processArray(arr, size);
-
-    free(arr);
+    
+    // Поиск простых элементов
+    findPrimes(arr, size);
+    
+    // Вывод измененного массива
+    printf("Измененный массив: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
     return 0;
 }
